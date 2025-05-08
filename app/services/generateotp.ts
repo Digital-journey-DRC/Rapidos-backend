dd8baad50daa8e411c7b3613a905c981e084b9a6
Jumvi <judahmvi@gmail.com>
2025-05-08 23:03:46 +0100

    fix: update OTP expiration logic to use native Date object instead of Luxon

--- app/services/generateotp.ts
+++ app/services/generateotp.ts
@@ -1,7 +1,5 @@
-import { DateTime } from 'luxon'
-
 export const generateOtp = () => {
   const otpCode = Math.floor(100000 + Math.random() * 900000)
-  const otpExpiredAt = DateTime.now().plus({ minutes: 10 }) // 10 minutes expiration
+  const otpExpiredAt = new Date(Date.now() + 50 * 60 * 1000) // 10 minutes expiration
   return { otpCode, otpExpiredAt }
 }
