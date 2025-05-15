313e7ee61caa4fd3873648cc051987f441e94c3a
Jumvi <judahmvi@gmail.com>
2025-05-15 09:17:17 +0100

    feat: initialize project with AdonisJS setup
    - Added package.json for project dependencies and scripts.
    - Created env.js for environment variable management.
    - Implemented kernel.js to set up middleware and error handling.
    - Defined routes in routes.js for basic API response.
    - Set up test bootstrap for Japa testing framework.

---
+++ build/start/routes.js
@@ -0,0 +1,7 @@
+import router from '@adonisjs/core/services/router';
+router.get('/', async () => {
+    return {
+        hello: 'world  i am rapidos',
+    };
+});
+//# sourceMappingURL=routes.js.map
