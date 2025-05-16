b70d66d356489ff93054ac252c9270d83d2f7b53
Jumvi <judahmvi@gmail.com>
2025-05-16 19:21:17 +0100

    feat: implement product creation logic with validation

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -1,3 +1,32 @@
-//import type { HttpContext } from '@adonisjs/core/http'
+import Media from '#models/media'
+import Product from '#models/product'
+import { createProductValidator } from '#validators/products'
+import type { HttpContext } from '@adonisjs/core/http'
 
-export default class ProductsController {}
+export default class ProductsController {
+  async index({ request, response, auth,params }: HttpContext) {
+      const user = auth.user!
+      const idVendeur = params.userId
+    if (!user) {
+      return response
+        .status(401)
+        .json({ message: "Vous n'etes pas authoriser à faire cette actions" })
+    }
+    try {
+        const payload = await request.validateUsing(createProductValidator)
+        const isProductExist = await Product.findBy('name', payload.name)
+        if(isProductExist?.vendeurId !== idVendeur){
+            return response.status(401).json({ message: "Ce produit existe déjà" })
+        }
+        const product = await Product.create({ ...payload, vendeurId: idVendeur })
+        const productMedia = request.input('media')
+        if (productMedia) {
+            await Media.createMany({})
+            await product.related('media').attach(productMedia.map((media: any) => media.id))
+        return response.status(201).json({ message: 'Produit créé avec succès', product })
+       
+    } catch (error) {
+        
+    }
+  }
+}
---
+++ app/validators/products.ts
@@ -0,0 +1,11 @@
+import vine from '@vinejs/vine'
+
+export const createProductValidator = vine.compile(
+  vine.object({
+    name: vine.string().trim().escape().minLength(2).maxLength(50),
+    description: vine.string().trim().escape().minLength(2).maxLength(500),
+    price: vine.number().positive(),
+    stock: vine.number().positive(),
+    category: vine.string().trim().escape().minLength(2).maxLength(50),
+  })
+)
