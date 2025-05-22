b23467c5ac0e30028a77fe8e90c8512d7e53660f
Jumvi <judahmvi@gmail.com>
2025-05-22 22:35:08 +0100

    feat: implement category management with create and delete functionalities; add category validation

---
+++ app/validators/category.ts
@@ -0,0 +1,9 @@
+import vine from '@vinejs/vine'
+import { ProductCategory } from '../Enum/product_category.js'
+
+export const categoryValidator = vine.compile(
+  vine.object({
+    name: vine.enum(ProductCategory),
+    description: vine.string().trim().escape().minLength(2).maxLength(500),
+  })
+)
